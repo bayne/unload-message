4e6d14b99a1b6ee45e3b2e1009ff339a62b46d7d
Brian Payne <bpayne@uci.edu>
2017-01-27 10:40:20 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var jquery = require('jquery');
+
+module.exports = function (confirmationMessage) {
+    jquery.ready(function() {
+        // We do not want this warning to trigger if the user is submitting a form
+        var formSubmitting = false;
+
+        $('[data-disable-onunload-message]').click(function() {
+            formSubmitting = true;
+        });
+
+        /*
+         * Logic for this from:
+         * http://stackoverflow.com/questions/7317273/warn-user-before-leaving-web-page-with-unsaved-changes
+         */
+        window.onload = function() {
+            window.beforeUnloadListener = function (e) {
+                if (formSubmitting) {
+                    return undefined;
+                }
+
+                // Gecko + IE
+                (e || window.event).returnValue = confirmationMessage;
+
+                // Gecko + Webkit, Safari, Chrome etc.
+                return confirmationMessage;
+            };
+            window.addEventListener("beforeunload", window.beforeUnloadListener);
+        };
+    });
+};
